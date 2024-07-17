b8b95580d1d24cd63d0a1379f77fc95a794eca89
Nathan Stanley <nstanley@bandwidth.com>
2024-07-17 11:48:59 -0400

    reactify: Adding packages

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 8000,
+  },
+  build: {
+    target: "es2019",
+    emptyOutDir: true,
+    outDir: "dist",
+  },
+});
