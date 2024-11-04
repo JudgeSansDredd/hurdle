bcd1d941872657bc36cc73ef209ba74ca2fa4ccd
Nathan Stanley <nstanley@bandwidth.com>
2024-11-04 14:58:59 -0500

    reactify: Some work on grabbing local state

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import Layout from "./layouts";
+import { getGameState } from "./utils/functions";
 
 export default function App() {
+  const gameState = getGameState();
+
   return (
     <Layout>
+      <div>{JSON.stringify(gameState)}</div>
       <div id="grid" className="grid grid-cols-2">
         <div>Possible remaining:</div>
         <div id="possible">-----</div>
